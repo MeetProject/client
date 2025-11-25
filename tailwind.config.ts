8d1fa6d654331ca16c958630193f1b8a322330c2
armd482 <armd482@naver.com>
2025-11-25 19:12:33 +0900

    chore: middleware proxy로 변경

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -78,12 +78,12 @@ const config: Config = {
         },
       },
       screens: {
-        '2xl': { max: '1535px' },
-        lg: { max: '1023px' },
-        md: { max: '800px' },
-        sm: { max: '480px' }, 
-        'sm-md': { max: '600px' }, 
-        xl: { max: '1279px' },
+        '2xl': '1535px',
+        lg: '1023px',
+        md: '800px',
+        sm: '480px', 
+        'sm-md': '600px', 
+        xl: '1279px',
       },
       transformOrigin: {
         'top-right': '100% 0%',
